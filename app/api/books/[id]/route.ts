03df499938b8359f7648dfc2fe89183d5884c253
chongqiangchen <chongqiangchen@gmail.com>
2024-04-27 17:42:18 +0800

    fix: fix vercel deploy

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -1,22 +1,5 @@
 import {NextRequest, NextResponse} from "next/server";
 
-export async function GET(
-    request: NextRequest,
-    {params}: {params: {id: string}}
-) {
-    const bookId = params.id;
-    try {
-        const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/books/" + bookId);
-        const books = await res.json();
-        return NextResponse.json(books, {status: 200});
-    } catch (error) {
-        console.error("Failed to fetch books", {error});
-        return NextResponse.json(
-            {message: "Internal server error"},
-            {status: 500}
-        );
-    }
-}
 
 export async function PUT(
     request: NextRequest,
@@ -52,6 +35,24 @@ export async function DELETE(
         const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/books/" + bookId, {
             method: "DELETE",
         });
+        const books = await res.json();
+        return NextResponse.json(books, {status: 200});
+    } catch (error) {
+        console.error("Failed to fetch books", {error});
+        return NextResponse.json(
+            {message: "Internal server error"},
+            {status: 500}
+        );
+    }
+}
+
+export async function GET(
+    request: NextRequest,
+    {params}: {params: {id: string}}
+) {
+    const bookId = params.id;
+    try {
+        const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/books/" + bookId);
         const books = await res.json();
         return NextResponse.json(books, {status: 200});
     } catch (error) {
