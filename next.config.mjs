e60bb2cab9b14eaf02152374a92b021b6dbc0137
chongqiangchen <chongqiangchen@gmail.com>
2024-04-27 17:42:18 +0800

    fix: fix vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['img14.360buyimg.com'],
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
